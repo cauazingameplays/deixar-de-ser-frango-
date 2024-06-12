77571ba9e38dadd91054f51194096e5b033f2ddf
caua de  oliveira queiroz <102962802+cauazingameplays@users.noreply.github.com>
2024-06-12 10:46:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const perguntas = [
+    {
+        enunciado: "Bem-vindo à Jornada de Gaia! Suas escolhas irão moldar o destino do planeta Terra. Faça suas decisões e veja o impacto no meio ambiente . Gaia, o espírito da Terra, precisa de sua ajuda para restaurar o equilibrio ambiental.",
+        alternativas: [
+            "Proteger as florestas",
+            "Conservar os oceanos"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
