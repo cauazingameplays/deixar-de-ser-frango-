2ecb0676a5538b3d0a31368a8d4d81cc2d7f1a8d
caua de  oliveira queiroz <102962802+cauazingameplays@users.noreply.github.com>
2024-07-31 10:52:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativa")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
 const perguntas = [
     {
         enunciado: "Bem-vindo à Jornada de Gaia! Suas escolhas irão moldar o destino do planeta Terra. Faça suas decisões e veja o impacto no meio ambiente . Gaia, o espírito da Terra, precisa de sua ajuda para restaurar o equilibrio ambiental.",
