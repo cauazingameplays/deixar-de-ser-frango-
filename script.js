2f4634354037817c411f4f0c5ca3053c519068e1
caua de  oliveira queiroz <102962802+cauazingameplays@users.noreply.github.com>
2024-08-14 10:48:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,10 +12,31 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Você escolheu proteger a floresta. Gaia te leva à amazônia",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Organizar uma campanha de consciêntização",
+            "Criar reservas florestais com a comunidade local"
         ]
     }
+    {
+        enunciado: "Sua campanha aumenta a consciêntização e ajuda a criar novas leis de proteção",
+        alternativas: [
+            "Expandir a campanha",
+            "Focar na educação ambiental"
+        ]
+    },
+    {
+        enunciado: "Bem-vindo à Jornada de Gaia! Suas escolhas irão moldar o destino do planeta Terra. Faça suas decisões e veja o impacto no meio ambiente . Gaia, o espírito da Terra, precisa de sua ajuda para restaurar o equilibrio ambiental.",
+        alternativas: [
+            "Proteger as florestas",
+            "Conservar os oceanos"
+        ]
+    },
+    {
+        enunciado: "Bem-vindo à Jornada de Gaia! Suas escolhas irão moldar o destino do planeta Terra. Faça suas decisões e veja o impacto no meio ambiente . Gaia, o espírito da Terra, precisa de sua ajuda para restaurar o equilibrio ambiental.",
+        alternativas: [
+            "Proteger as florestas",
+            "Conservar os oceanos"
+        ]
+    },
 ]; 
